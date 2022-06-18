548b39f7e0f8ef3142cdb688e57d2f97c7c2833b
Leonardo Jesus Braz <101673432+LeonardoBraz1@users.noreply.github.com>
2022-06-18 00:24:33 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,17 @@
+const calcular = document.getElementById("calcular");
+
+function imc() {
+  const nome = document.getElementById("nome").value;
+  const altura = document.getElementById("altura").value;
+  const peso = document.getElementById("peso").value;
+  const resultado = document.getElementById("resultado");
+
+  if (nome !== "" && altura !== "" && peso !== "") {
+    const valorimc = peso / (altura * altura).toFixed(1);
+    resultado.textContent = valorimc.toFixed(1);
+  } else {
+    resultado.textContent = "Preencha todos os campos!!!";
+  }
+}
+
+calcular.addEventListener("click", imc);
